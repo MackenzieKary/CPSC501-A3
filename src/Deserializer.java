b2081a1a72f1b2f01a5fc7e23c8f0b2198ab5f14
Mackenzie Kary <mackenziekary@192.168.0.147>
2017-11-13 19:38:42 -0700

    Beginning deserialization method

--- src/Deserializer.java
+++ src/Deserializer.java
@@ -7,10 +7,17 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.List;
+
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 public class Deserializer {
 
-	public static void main(String[] args) throws UnknownHostException, IOException{
+	public static void main(String[] args) throws UnknownHostException, IOException, JDOMException{
 		ServerSocket ss = new ServerSocket(9998);
 		System.out.println("Open Connection...");
 		//Socket connectedSocket = new Socket("localhost", 9998);
@@ -24,5 +31,42 @@ public class Deserializer {
 		connectedSocket.close();
 		ss.close();
 		
+		
+		/*
+		 * Start of deserialization
+		 */
+		File inputFile = new File("docReceived.xml");
+			
+		
+		SAXBuilder saxBuilder = new SAXBuilder();
+		
+		Document document  = saxBuilder.build(inputFile);
+		
+		deserialize(document); 
+	}
+	
+	public static Object deserialize(Document document){
+		Element classElement = document.getRootElement();	
+		
+		List<Element> studentList = classElement.getChildren();
+		
+		
+		/* EXAMPLE FROM TUTORIAL:
+		 * Element classElement = document.getRootElement();	
+			
+			List<Element> studentList = classElement.getChildren();
+			System.out.println("------------------------");
+			
+			for(int i = 0; i < studentList.size(); i++){
+				Element student = studentList.get(i);
+				System.out.println("\nCurrent Element: "+ student.getName());
+				Attribute attribute = student.getAttribute("id");
+				System.out.println("Student id: " +attribute.getValue());
+				System.out.println("First name: "+ student.getChild("firstName").getText());
+				System.out.println("Last name: "+ student.getChild("lastName").getText());
+				System.out.println("Grade: "+ student.getChild("marks").getText());			
+			}
+		 */
+		return null;
 	}
 }

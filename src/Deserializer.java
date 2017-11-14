2b7d4425c5a6935975e5464020831773ddcacc32
Mackenzie Kary <mackenziekary@192.168.0.147>
2017-11-13 18:50:53 -0700

    File transfer working, but serializer isn't working correctly (not serializing entire object)

--- src/Deserializer.java
+++ src/Deserializer.java
@@ -1,5 +1,6 @@
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,12 +12,17 @@ public class Deserializer {
 
 	public static void main(String[] args) throws UnknownHostException, IOException{
 		ServerSocket ss = new ServerSocket(9998);
-		System.out.println("Before accept");
-		Socket s = ss.accept();
-		System.out.println("After accept");
-		DataInputStream dis = new DataInputStream(s.getInputStream());
-		System.out.println("After DIS");
-		System.out.println("Dis = " + dis);
-		System.out.println(dis.readUTF());
+		System.out.println("Open Connection...");
+		//Socket connectedSocket = new Socket("localhost", 9998);
+		Socket connectedSocket = ss.accept();
+		System.out.println("Connection Accepted");
+		byte[] buffer = new byte[99999]; // Set byte array to something large
+		int inputBytes = connectedSocket.getInputStream().read(buffer, 0, buffer.length);
+		BufferedOutputStream buffStream = new BufferedOutputStream(new FileOutputStream("docReceived.xml"));
+		buffStream.write(buffer, 0, inputBytes);
+		buffStream.close();
+		connectedSocket.close();
+		ss.close();
+		
 	}
 }
